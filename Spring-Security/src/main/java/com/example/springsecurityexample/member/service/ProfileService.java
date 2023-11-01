eb0e1f8344ce3f5838499f0b9f3c24c5b86e8938
sehwan2 <tgc03151@gmail.com>
2023-11-01 22:29:07 +0900

    Profile Contorller, ProfileService 틀 완성

--- Spring-Security/src/main/java/com/example/springsecurityexample/member/service/ProfileService.java
+++ Spring-Security/src/main/java/com/example/springsecurityexample/member/service/ProfileService.java
@@ -1,5 +1,6 @@
 package com.example.springsecurityexample.member.service;
 
+import com.example.springsecurityexample.member.Profile;
 import com.example.springsecurityexample.member.dto.ProfileRequest;
 import com.example.springsecurityexample.member.dto.ProfileResponse;
 import com.example.springsecurityexample.member.repository.ProfileRepository;
@@ -14,22 +15,64 @@ public class ProfileService {
 
     private final ProfileRepository profileRepository;
 
+
     public ProfileResponse getProfile(Long id) {
-        // 실제 프로필을 데이터베이스에서 검색하여 ProfileResponse 객체로 변환하는 로직을 구현
-        return new ProfileResponse();
+        Profile profile = profileRepository.findById(id).orElse(null);
+        if (profile == null) {
+            // 해당 ID의 프로필이 존재하지 않으면 예외 처리
+            throw new ProfileNotFoundException("프로필을 찾을 수 없습니다. ID: " + id);
+        }
+        // Profile 엔티티를 ProfileResponse DTO로 변환하는 메서드를 호출
+        return convertToResponse(profile);
     }
 
     public ProfileResponse createProfile(ProfileRequest request) {
-        // 실제 프로필을 데이터베이스에 저장하고, 생성된 프로필을 ProfileResponse 객체로 변환하는 로직을 구현
-        return new ProfileResponse();
+        // ProfileRequest DTO를 Profile 엔티티로 변환
+        Profile profile = convertToEntity(request);
+        // Profile 엔티티를 데이터베이스에 저장
+        Profile savedProfile = profileRepository.save(profile);
+        // 저장된 프로필 엔티티를 ProfileResponse DTO로 변환
+        return convertToResponse(savedProfile);
     }
 
     public ProfileResponse updateProfile(Long id, ProfileRequest request) {
-        // 실제 프로필을 데이터베이스에서 업데이트하고, 업데이트된 프로필을 ProfileResponse 객체로 변환하는 로직을 구현
-        return new ProfileResponse();
+        Profile profile = profileRepository.findById(id).orElse(null);
+        if (profile == null) {
+            // 해당 ID의 프로필이 존재하지 않으면 예외 처리
+            throw new ProfileNotFoundException("프로필을 찾을 수 없습니다. ID: " + id);
+        }
+        // ProfileRequest DTO를 Profile 엔티티로 업데이트
+        updateEntityFromRequest(profile, request);
+        // 업데이트된 프로필 엔티티를 데이터베이스에 저장
+        Profile updatedProfile = profileRepository.save(profile);
+        // 업데이트된 프로필 엔티티를 ProfileResponse DTO로 변환
+        return convertToResponse(updatedProfile);
     }
 
     public void deleteProfile(Long id) {
-        // 실제 프로필을 데이터베이스에서 삭제하는 로직을 구현
+        Profile profile = profileRepository.findById(id).orElse(null);
+        if (profile == null) {
+            // 해당 ID의 프로필이 존재하지 않으면 예외 처리
+            throw new ProfileNotFoundException("프로필을 찾을 수 없습니다. ID: " + id);
+        }
+        // 데이터베이스에서 프로필 삭제
+        profileRepository.delete(profile);
+    }
+
+    // Profile 엔티티를 ProfileResponse DTO로 변환하는 메서드
+    private ProfileResponse convertToResponse(Profile profile) {
+        // 변환 로직을 구현
+        return new ProfileResponse();
+    }
+
+    // ProfileRequest DTO를 Profile 엔티티로 변환하는 메서드
+    private Profile convertToEntity(ProfileRequest request) {
+        // 변환 로직을 구현
+        return new Profile();
+    }
+
+    // Profile 엔티티를 ProfileRequest DTO로 업데이트하는 메서드
+    private void updateEntityFromRequest(Profile profile, ProfileRequest request) {
+        // 업데이트 로직을 구현
     }
 }
